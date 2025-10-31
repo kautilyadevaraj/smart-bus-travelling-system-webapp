5303915ccfe1999065afab8968cc816b21b84cef
kautilyadevaraj <kautilyadk@gmail.com>
2025-10-31 10:49:53 +0530

    changed global font to figtree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Figtree } from "next/font/google";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { MobileNav } from "@/components/mobile-nav";
 import "./globals.css";
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const figtree = Figtree({
+  subsets: ["latin"],
+  variable: "--font-figtree",
+});
 
 export const metadata: Metadata = {
   title: "SmartRide Bus Dashboard",
@@ -22,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans antialiased`}>
+      <body className={` ${figtree.variable} antialiased`}>
         {/* Desktop: SidebarProvider with AppSidebar */}
         <SidebarProvider className="hidden md:flex">
           <AppSidebar />
@@ -31,7 +33,7 @@ export default function RootLayout({
 
         {/* Mobile: MobileNav only */}
         <div className="md:hidden flex flex-col h-screen">
-            <MobileNav />
+          <MobileNav />
 
           <main className="flex-1 overflow-auto">{children}</main>
         </div>
